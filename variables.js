b095b0a0c2e75f3e0c744c20a9143bb3e47ecf8c
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-07-28 10:47:04 -0500

    Reassign string variables and add console.logs for string console output

--- variables.js
+++ variables.js
@@ -14,3 +14,14 @@ let numberOfFingers = 10;
 let isHungry = false;
 let isSleepy = true;
 let isOverEncumbered = false;
+
+// Reassign string vars
+firstName = "Beauregard";
+lastName = "Cumberflaps";
+email = "sherlockisstrange@netscape.com";
+
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
