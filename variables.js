25c66078ee0cc2475dd019b7473715eaad17231d
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-07-28 10:32:04 -0500

    Add number variables to variables.js

--- variables.js
+++ variables.js
@@ -4,3 +4,8 @@
 let firstName = "Eric";
 let lastName = "Kendrick";
 let email = "eric.c.kendrick@gmail.com";
+
+//Number variables
+let age = 41;
+let numberOfSiblings = 1;
+let numberOfFingers = 10;
